10c0a35228213bbde59f21d9f29bac1f4e3dcffe
Raffael Sahli <raffael.sahli@doodle.com>
2025-04-16 10:13:43 +0200

    feat: support roles in KeycloakClient, #403

--- internal/controllers/keycloakrealm_controller.go
+++ internal/controllers/keycloakrealm_controller.go
@@ -706,6 +706,15 @@ func (r *KeycloakRealmReconciler) extendRealmWithClients(ctx context.Context, re
 			client.Spec.Client.ClientID = client.Name
 		}
 
+		if realm.Spec.Realm.Roles == nil {
+			realm.Spec.Realm.Roles = &infrav1beta1.RolesRepresentation{}
+		}
+
+		if len(client.Spec.Client.Roles) > 0 {
+			realm.Spec.Realm.Roles.Client[client.Spec.Client.ClientID] = append(realm.Spec.Realm.Roles.Client[client.Spec.Client.ClientID], client.Spec.Client.Roles...)
+			client.Spec.Client.Roles = nil
+		}
+
 		realm.Spec.Realm.Clients = append(realm.Spec.Realm.Clients, client.Spec.Client)
 	}
 
